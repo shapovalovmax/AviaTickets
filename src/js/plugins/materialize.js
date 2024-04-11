01082b06d3c2d3b1ad172fdc999ba87981202f7a
shapovalovmax <dramixwow@gmail.com>
2024-04-11 11:51:36 +0300

    Added UI

---
+++ src/js/plugins/materialize.js
@@ -0,0 +1,36 @@
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
+
+// Init select
+const select = document.querySelectorAll('select');
+M.FormSelect.init(select);
+
+export function getSelectInstance(elem) {
+    return M.FormSelect.getInstance(elem);
+}
+
+// Init Autocomplete
+const autocomplete = document.querySelectorAll('.autocomplete');
+M.Autocomplete.init(autocomplete,
+    {
+        data: {
+            "Apple": null,
+            "Microsoft": null,
+            "Google": 'https://placehold.it/250x250'
+        }
+    }
+);
+
+export function getAutocompleteInstance(elem) {
+    return M.Autocomplete.getInstance(elem);
+}
+
+//Init datePickers
+const datePickers = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datePickers, {
+    showClearBtn: true
+});
+
+export function getDatePickersInstance(elem) {
+    return M.Datepicker.getInstance(elem);
+}
